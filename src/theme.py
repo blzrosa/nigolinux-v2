879dbb5189d1318565ef525416e05f8eb10305c8
blzrosa <vblzrosa@gmail.com>
2025-11-10 14:20:23 -0300

    mend
    added theme install in theme, changed " to ' when possible

--- src/theme.py
+++ src/theme.py
@@ -3,11 +3,11 @@ from src.utils.install import install
 from typing import Dict
 
 SETTINGS: Dict[str, str] = {
-    "gtk-theme": "catppuccin-mocha-mauve-standard+default",
-    "icon-theme": "Papirus-Dark",
-    "cursor-theme": "Bibata-Modern-Classic",
-    "font-name": "Noto Sans Regular 11",
-    "color-scheme": "prefer-dark",
+    'gtk-theme': 'catppuccin-mocha-mauve-standard+default',
+    'icon-theme': 'Papirus-Dark',
+    'cursor-theme': 'Bibata-Modern-Classic',
+    'font-name': 'Noto Sans Regular 11',
+    'color-scheme': 'prefer-dark',
 }
 
 def apply_gnome_settings() -> None:
@@ -16,4 +16,4 @@ def apply_gnome_settings() -> None:
         'yay': ['catppuccin-gtk-theme-mocha', 'papirus-icon-theme', 'bibata-cursor-theme',],
         })
     for setting, value in SETTINGS.items():
-        execute_as_user(["set", "org.gnome.desktop.interface", setting, value])
+        execute_as_user(['set', 'org.gnome.desktop.interface', setting, value])
