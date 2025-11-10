7101195e1ba9485bc6cc27bd84793466410f1bb1
blzrosa <vblzrosa@gmail.com>
2025-11-10 10:57:57 -0300

    added progress bar utility

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+BASE_PATH: Path = Path(__file__).resolve().parent.parent
+SRC_PATH: Path = BASE_PATH / 'src'
+ASSETS_PATH: Path = BASE_PATH / 'assets'
+
+if __name__ == '__main__':
+    print('BASE_PATH:', BASE_PATH)
+    print('SRC_PATH:', SRC_PATH)
+    print('ASSETS_PATH', ASSETS_PATH)
