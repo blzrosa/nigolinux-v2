3782f81e73d2df2ed90bf13cb4b92e43545a0772
blzrosa <vblzrosa@gmail.com>
2025-11-10 23:56:46 -0300

    added home path resolver, added dotfiles configuration file

---
+++ src/dotfiles.py
@@ -0,0 +1,35 @@
+from src import HOME_PATH, ASSETS_PATH
+from src.utils.permissions import get_sudo_user
+from src.utils.permissions import add_permissions, FilePermissions
+from pathlib import Path
+from typing import List
+import shutil
+import os
+import pwd
+
+SOURCE_CONFIG: Path = ASSETS_PATH / '.config'
+
+def apply_dotfiles() -> None:
+    dest_config: Path = HOME_PATH / '.config'
+    backup_config: Path = HOME_PATH / '.config.old'
+
+    if not SOURCE_CONFIG.is_dir():
+        raise FileNotFoundError
+    if dest_config.exists():
+        shutil.rmtree(backup_config)
+    shutil.move(dest_config, backup_config)
+    shutil.copytree(SOURCE_CONFIG, dest_config)
+    user_info = pwd.getpwnam(get_sudo_user())
+    uid, gid = user_info.pw_uid, user_info.pw_gid
+    os.chown(dest_config, uid, gid)
+    for root, dirs, files in os.walk(dest_config):
+        for d in dirs:
+            os.chown(os.path.join(root, d), uid, gid)
+        for f in files:
+            os.chown(os.path.join(root, f), uid, gid)
+    script_dirs_patterns: List[str] = [
+        str(HOME_PATH / '.config/hypr/scripts/*.sh'),
+        str(HOME_PATH / '.config/waybar/scripts/*.sh'),
+    ]
+    add_permissions(script_dirs_patterns, (FilePermissions.execute,))
+    return None
