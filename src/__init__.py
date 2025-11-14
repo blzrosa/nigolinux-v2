31a83371a70c919e63e9be70ff4e4258353cd436
Rosa <90849953+blzrosa@users.noreply.github.com>
2025-11-14 19:52:20 -0300

    Update HOME_PATH to use environment variables

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+import os
 
 BASE_PATH: Path = Path(__file__).resolve().parent.parent
 SRC_PATH: Path = BASE_PATH / "src"
 ASSETS_PATH: Path = BASE_PATH / "assets"
-HOME_PATH: Path = Path("~").expanduser()
+HOME_PATH: Path = Path(os.path.expanduser(f'~{os.getenv('SUDO_USER') or os.getenv('USERNAME')}'))
 
 if __name__ == "__main__":
     print("BASE_PATH:", BASE_PATH)
